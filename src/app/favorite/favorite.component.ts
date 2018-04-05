161ca711d7a0865f607b9531a8936cc2edecbf0a
RACHIT Bhatia <rachit.bhatia@ibm.com>
2018-04-05 14:07:09 -0400

    custom pile for title cases

---
+++ src/app/favorite/favorite.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'favorite',
+  templateUrl: './favorite.component.html',
+  styleUrls: ['./favorite.component.css']
+})
+export class FavoriteComponent implements OnInit {
+
+  isFavourite: boolean;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onClick(){
+    this.isFavourite = !this.isFavourite;
+  }
+}
